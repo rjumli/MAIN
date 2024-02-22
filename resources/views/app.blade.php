5128ff0d09b93acee82ea8668b3a94b8e1a5aabf
kradworkz <80160702+kradworkz@users.noreply.github.com>
2024-02-22 09:11:06 +0800

    asda

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -14,8 +14,10 @@
     <meta property="og:url" content="URL to the template's webpage">
     <meta name="twitter:card" content="summary_large_image">
     <link rel="shortcut icon" href="{{ URL::asset('image/sei.ico') }}">
-    
-    @vite(['resources/js/app.js', "resources/js/Pages/{$page['component']}.vue"])
+    <link rel="stylesheet" href="{{ asset('build/css/app.css') }}">
+<script src="{{ asset('build/js/app.js') }}" defer></script>
+
+    <!-- @vite(['resources/js/app.js', "resources/js/Pages/{$page['component']}.vue"]) -->
     @inertiaHead
 </head>
 
